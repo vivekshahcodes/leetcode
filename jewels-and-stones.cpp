a9aa47f1957da610e97945582fedea3da2574a36
Vivek Shah <vivekshahcodes@gmail.com>
2021-05-30 15:56:20 +0530

    Add jewels-and-stones.cpp

--- jewels-and-stones.cpp
+++ jewels-and-stones.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         
-        int ans = 0 ;
+        int ans = 0;
         for(int i=0;i<stones.length();i++){
             for(int j=0;j<jewels.length();j++){
                 if(stones[i]==jewels[j]){
