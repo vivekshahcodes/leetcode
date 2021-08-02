1d9c7d11db6e6a25f4b815f3375d674c7777b06b
Vivek Shah <vivekshahcodes@gmail.com>
2021-08-02 11:56:22 +0530

    Update running-sum-of-1d-array.cpp

--- running-sum-of-1d-array.cpp
+++ running-sum-of-1d-array.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         
-        vector<int> v(nums.size());
-        v[0] = nums[0];
-        
-        for(int i=1; i<nums.size(); i++){
-            v[i] = v[i-1] + nums[i];
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
         }
         
-        return v;
+        return nums;
     }
 };
