02fbae8dd1283552f0a5f6cc9aee26ce54928bfe
Vivek Shah <vivekshahcodes@gmail.com>
2021-08-04 18:37:15 +0530

    Create counting-bits.cpp

---
+++ counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> v;
+        
+        for(int i=0;i<=n;i++){
+            v.push_back(__builtin_popcount(i));
+        }
+        
+        return v;
+    }
+};
