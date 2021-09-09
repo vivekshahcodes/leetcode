587aad62b91787fd9dbb5a95d0cf29a0cae601a8
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-09 20:22:45 +0530

    Update power-of-two.cpp

--- power-of-two.cpp
+++ power-of-two.cpp
@@ -2,7 +2,18 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-        if(__builtin_popcount(n)==1 && n>0){
+        if(n<0){
+            return false;
+        }
+        
+        int ans = 0;
+        
+        while(n>0){
+            ans++;
+            n &= n-1;
+        }
+        
+        if(ans==1){
             return true;
         }
         
