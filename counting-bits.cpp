566e45ca2c3e9a1a0e4c7d7ec476891481a951af
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-09 20:17:59 +0530

    Update counting-bits.cpp

--- counting-bits.cpp
+++ counting-bits.cpp
@@ -5,7 +5,16 @@ public:
         vector<int> v;
         
         for(int i=0;i<=n;i++){
-            v.push_back(__builtin_popcount(i));
+            
+            int ans = 0;
+            int x = i;
+            
+            while(x>0){
+                ans++;
+                x &= x-1;
+            }
+            
+            v.push_back(ans);
         }
         
         return v;
