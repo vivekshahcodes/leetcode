785b1c4184899c25a8240c8c68c7482296fbbfaa
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-19 11:44:01 +0530

    Update longest-substring-without-repeating-characters.cpp

--- longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         
-        map<char,int> m;
+        unordered_map<char,int> m;
         int ans = 0, curr = 0;
         
         for(int i=0;i<s.length();i++){
