2a836d931722411db127929febba0e2f182598bb
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-23 22:15:48 +0530

    Create final-value-of-variable-after-performing-operations.cpp

---
+++ final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
+        int x = 0;
+        
+        for(string i:operations){
+            if(i[1]=='+'){
+                x++;
+            }else{
+                x--;
+            }
+        }
+        
+        return x;
+    }
+};
