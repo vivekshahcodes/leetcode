8bc1c474d29f471c3ecbabe0cc46b15a786a8d17
Vivek Shah <vivekshahcodes@gmail.com>
2021-10-20 19:16:58 +0530

    Create split-a-string-in-balanced-strings.cpp

---
+++ split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        
+        int l = 0, r = 0, ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            if(s[i] == 'L')
+                l++;
+            else
+                r++;
+            
+            if(l == r){
+                
+                ans++;
+                l = 0;
+                r = 0;
+            }
+                
+        }
+        
+        return ans;
+    }
+};
