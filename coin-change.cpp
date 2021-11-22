28660bbd176ce595524dd1ab2d5770e1f89c53ab
Vivek Shah <vivekshahcodes@gmail.com>
2021-11-23 00:33:12 +0530

    Update coin-change.cpp

--- coin-change.cpp
+++ coin-change.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     
-    int dp[10001] = {0};
-    
-    int minCoins(vector<int>& coins, int amount){
-        
-        if(amount == 0){
-            return 0;
-        }
-        
-        if(dp[amount] != 0){
-            return dp[amount];
-        }
-        
-        int ans = 10001;
-        
-        for(int i = 0; i < coins.size(); i++){
-            
-            if(amount - coins[i] >= 0){
-                int subProblem = minCoins(coins, amount - coins[i]);
-                ans = min(ans, subProblem+1);
-            }
-            
-        }
-        
-        dp[amount] = ans;
-        
-        return dp[amount];
-        
-    }
-    
     int coinChange(vector<int>& coins, int amount) {
         
-        int ans = minCoins(coins, amount);
+        int dp[10001] = {0};
         
-        if(ans == 10001){
+        for(int i = 1; i <= amount; i++){
+            
+            int curr = 10001;
+            
+            for(int j = 0; j < coins.size(); j++){
+                
+                if(i - coins[j] >= 0){
+                    
+                    curr = min(curr, dp[i - coins[j]]);
+                }
+            }
+                        
+            dp[i] = curr + 1;
+            
+        }
+        
+        if(dp[amount] > 10000){
             return -1;
         }
         
-        return ans;
+        return dp[amount];
         
     }
 };
