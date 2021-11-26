9bdee94918a66090607cafa7f0eb164c4e0eb226
Vivek Shah <vivekshahcodes@gmail.com>
2021-11-26 20:49:40 +0530

    Update search-insert-position.cpp

--- search-insert-position.cpp
+++ search-insert-position.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         
-        int l = 0, r = nums.size()-1, ans;
+        int l = 0, r = nums.size()-1;
         
         while(l<=r){
             
