8942c068857a9460bed640d10055e8544b0a0061
Vivek Shah <vivekshahcodes@gmail.com>
2021-12-29 17:22:36 +0530

    Create first-bad-version.cpp

---
+++ first-bad-version.cpp
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        int l = 0, r = n;
+        
+        while(l<r-1){
+            
+            int m  = l + (r-l)/2;
+            
+            if(isBadVersion(m)){
+                r = m;
+            }else{
+                l = m;
+            }
+        }
+        
+        return r;
+    }
+};
