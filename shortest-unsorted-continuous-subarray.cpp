efeee0712e2888c3e6cff2befead44afa527a0d6
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-05 14:48:10 +0530

    Update shortest-unsorted-continuous-subarray.cpp

--- shortest-unsorted-continuous-subarray.cpp
+++ shortest-unsorted-continuous-subarray.cpp
@@ -1,65 +1,62 @@
 class Solution {
 public:
     
-    bool isOutOfOrder(vector<int>& nums, int i) {
+    bool isOutOfOrder(vector<int>& nums, int i){
 
         int n = nums.size();
 
-        if (i == 0) {
-            return nums[i] > nums[1];
-        } else if (i == n - 1) {
-            return nums[i] < nums[n - 2];
-        } else {
-            return nums[i] < nums[i - 1] || nums[i] > nums[i + 1];
+        if(i==0){
+            return nums[i]>nums[1];
+        }else if(i==n-1){
+            return nums[i]<nums[n-2];
+        }else{
+            return nums[i]<nums[i-1] || nums[i]>nums[i+1];
         }
     }
-
-    int findUnsortedSubarray(vector<int>& nums) {
+    
+    int findUnsortedSubarray(vector<int>& nums){
 
         int n = nums.size();
-        
-        if(n==1){
-            return 0;
-        }
+        if(n==1) return 0;
         
         int smallest = INT_MAX;
         int largest = INT_MIN;
 
-        for (int i = 0; i < n; i++) {
-            if (isOutOfOrder(nums, i)) {
+        for(int i=0;i<n;i++){
+            if(isOutOfOrder(nums,i)){
                 int x = nums[i];
-                smallest = min(smallest, x);
-                largest = max(largest, x);
+                smallest = min(smallest,x);
+                largest = max(largest,x);
             }
         }
 
-        if (smallest == INT_MAX) {
+        if(smallest==INT_MAX){
             return 0;
         }
 
-        int index, left, right;
+        int index,left,right;
         index = 0;
 
-        while (index < n) {
-            if (smallest >= nums[index]) {
+        while(index<n){
+            if(smallest>=nums[index]){
                 index++;
-            } else {
+            }else{
                 left = index;
                 break;
             }
         }
 
-        index = n - 1;
+        index = n-1;
 
-        while (index >= 0) {
-            if (largest <= nums[index]) {
+        while(index>=0){
+            if(largest<=nums[index]){
                 index--;
-            } else {
+            }else{
                 right = index;
                 break;
             }
         }
 
-        return right - left + 1;
+        return right-left+1;
     }
 };
