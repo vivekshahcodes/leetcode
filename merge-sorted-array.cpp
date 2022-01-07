ef11c680c685bf72d5f8541314f24c242e683837
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-07 22:05:15 +0530

    Update merge-sorted-array.cpp

--- merge-sorted-array.cpp
+++ merge-sorted-array.cpp
@@ -6,32 +6,23 @@ public:
         int i = m-1;
         int j = n-1;
         
-        while(i>=0 && j>=0 && ind>=0){
+        while(i>=0 && j>=0){
             
-            if(nums1[ind]!=0){
-                ind--;
-                continue;
-            }
-            
-            if(nums1[i]>nums2[j]){
-                swap(nums1[i],nums1[ind]);
-                i--;
-                ind--;
-            }else{
+            if(nums1[i]<nums2[j]){
                 nums1[ind] = nums2[j];
-                nums2[j] = 0;
                 ind--;
                 j--;
+            }else{
+                nums1[ind] = nums1[i];
+                ind--;
+                i--;
             }
         }
         
-        i = 0;
-        j = 0;
-        
-        while(j<n && nums2[j]!=0){
-            nums1[i] = nums2[j];
-            i++;
-            j++;
+        while(j>=0){
+            nums1[ind] = nums2[j];
+            ind--;
+            j--;
         }
     }
 };
