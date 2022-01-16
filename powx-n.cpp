7961da3ade339bfa29d44ce1988d8f3e721b76f6
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-17 02:35:26 +0530

    Create powx-n.cpp

---
+++ powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double res = 1;
+        
+        while(n){
+            
+            if(n&1){
+                if(n>0){
+                    res *= x;
+                }else{
+                    res /= x;
+                }
+            }
+            
+            x = x*x;
+            
+            n /= 2;
+        }
+        
+        return res;
+    }
+};
