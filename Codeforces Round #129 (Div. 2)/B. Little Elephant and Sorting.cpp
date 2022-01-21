67aa1d564d6c684c9c7698180eab329cfd9c8e88
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-21 18:00:24 +0530

    Create B. Little Elephant and Sorting.cpp

---
+++ Codeforces Round #129 (Div. 2)/B. Little Elephant and Sorting.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t=1;
+
+	while(t--){
+
+		ll n;
+		cin>>n;
+
+		vector<ll> a(n);
+
+		for(ll i=0;i<n;i++){
+			cin>>a[i];
+		}
+
+		ll ans = 0, i = 1;
+
+		while(i<n && a[i]>=a[i-1]){
+			i++;
+		}
+
+		while(i<n){
+			a[i] += ans;
+			if(a[i]>=a[i-1]){
+				i++;
+				continue;
+			}
+			ll x = a[i-1]-a[i];
+			ans += x;
+			a[i] += x;
+			i++;
+		}
+
+		cout<<ans;
+	}
+
+	return 0;
+}
