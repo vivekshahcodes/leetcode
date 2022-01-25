bb38a4290cf8b340e08e8d627700401b76141142
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-26 03:00:51 +0530

    Create find-peak-element.cpp

---
+++ find-peak-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+        int n = nums.size(), l = 0, r = n-1;
+        
+        while(l < r){
+            int m = l + (r - l) / 2;
+            if(nums[m] > nums[m + 1]){
+                r = m;
+            }else{
+                l = m + 1;
+            }
+        }
+        
+        return l;
+    }
+};
