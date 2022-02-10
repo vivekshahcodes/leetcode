75c5fe0e153b25e4ff9c40e5f534ea9a45500f9e
Vivek Shah <vivekshahcodes@gmail.com>
2022-02-11 02:41:50 +0530

    Create remove-element.cpp

---
+++ remove-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int n = nums.size(), index = 0;
+        
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]!=val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        
+        return index;
+    }
+};
