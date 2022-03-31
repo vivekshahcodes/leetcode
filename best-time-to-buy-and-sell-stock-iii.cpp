5ec210c1661108b4c4915b0d27d6d40380de516d
Vivek Shah <vivekshahcodes@gmail.com>
2022-04-01 03:35:53 +0530

    Update best-time-to-buy-and-sell-stock-iii.cpp

--- best-time-to-buy-and-sell-stock-iii.cpp
+++ best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     
-    int maximumProfit(int i, int n, vector<int>& prices, bool canBuy, int cap, vector<vector<vector<int>>>& dp){
+    int maximumProfit(int i, int n, vector<int>& prices, int trans, vector<vector<int>>& dp){
         
-        if(i==n || cap==0){
+        if(i==n || trans==5){
             return 0;
         }
         
-        if(dp[i][canBuy][cap]!=-1){
-            return dp[i][canBuy][cap];
+        if(dp[i][trans]!=-1){
+            return dp[i][trans];
         }
         
-        if(canBuy){
-            int pick = maximumProfit(i+1,n,prices,false,cap,dp)-prices[i];
-            int notPick = maximumProfit(i+1,n,prices,true,cap,dp);
-            return dp[i][canBuy][cap] = max(pick,notPick);
+        if(trans&1){
+            int pick = maximumProfit(i+1,n,prices,trans+1,dp)-prices[i];
+            int notPick = maximumProfit(i+1,n,prices,trans,dp);
+            return dp[i][trans] = max(pick,notPick);
         }
         
-        int pick = maximumProfit(i+1,n,prices,true,cap-1,dp)+prices[i];
-        int notPick = maximumProfit(i+1,n,prices,false,cap,dp);
+        int pick = maximumProfit(i+1,n,prices,trans+1,dp)+prices[i];
+        int notPick = maximumProfit(i+1,n,prices,trans,dp);
         
-        return dp[i][canBuy][cap] = max(pick,notPick);
+        return dp[i][trans] = max(pick,notPick);
     }
     
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
         
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3, -1)));
+        vector<vector<int>> dp(n, vector<int> (5,-1));
         
-        return maximumProfit(0,n,prices,true,2,dp);
+        return maximumProfit(0,n,prices,1,dp);
     }
 };
