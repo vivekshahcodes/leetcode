7d4c259bb5f6c03fa45479f49575928823c526e9
Vivek Shah <vivekshahcodes@gmail.com>
2022-06-24 13:54:53 +0530

    Update climbing-stairs.cpp

--- climbing-stairs.cpp
+++ climbing-stairs.cpp
@@ -1,22 +1,84 @@
+//Recursion
+//TC - O(2^n), more precisely, O(1.618^n)
+//SC - O(n) for the stack space
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+};
+
+
+//Memoization
+//TC - O(n)
+//SC - O(n) for the stack space + O(n) for the dp array
+
 class Solution {
 public:
     
-    int climbStairs(int n) {
+    int totalWays(int n, vector<int>& dp){
         
-        int a = 1, b = 1;
-        
-        if(n<=1){
-            return a;
+        if(n==0 || n==1){
+            return 1;
         }
         
-        int c;
+        if(dp[n] != -1){
+            return dp[n];
+        }
         
-        for(int i=1;i<n;i++){
-            c = a+b;
+        return totalWays(n-1,dp) + totalWays(n-2,dp);
+    }
+    
+    int climbStairs(int n) {
+        
+        vector<int> dp(n+1,-1);
+        return totalWays(n,dp);
+    }
+};
+
+
+//Tabulation
+//TC - O(n)
+//SC - O(n) for the dp array
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
+
+
+//Space Optimization
+//TC - O(n)
+//SC - O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int a = 1, b = 1, c = 1;
+        
+        for(int i=2;i<=n;i++){
+            c = a + b;
             a = b;
             b = c;
         }
         
-        return b;
+        return c;
     }
 };
