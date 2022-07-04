1e47d5bf6ea435b9fcfe98e31d0d6cf281fe9c28
Vivek Shah <vivekshahcodes@gmail.com>
2022-07-04 20:58:02 +0530

    Update majority-element.cpp

--- majority-element.cpp
+++ majority-element.cpp
@@ -1,25 +1,78 @@
+//Brute Force
+//TC - O(n^2)
+//SC - O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
         int n = nums.size();
-        int count = 0;
-        int element;
         
-        for(int i=0;i<n;i++){
-            
-            if(count==0){
-                element = nums[i];
-                count++;
-            }else{
-                if(nums[i]==element){
-                    count++;
-                }else{
-                    count--;
-                }
+        for(int x:nums){
+            int c = 0;
+            for(int y:nums){
+                if(x==y) c++;
             }
+            if(c>(n/2)) return x;
         }
         
-        return element;
+        return -1;
+    }
+};
+
+
+//Sorting Approach
+//TC - O(nlogn)
+//SC - O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[(n-1)/2];
+    }
+};
+
+
+//Hashmap Approach
+//TC - O(n)
+//SC - O(n) for the hashmap
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        unordered_map<int,int> m;
+        
+        for(int x:nums){
+            m[x]++;
+            if(m[x]>(n/2)) return x;
+        }
+        
+        return -1;
+    }
+};
+
+
+//Boyer-Moore Voting Algorithm
+//TC - O(n)
+//SC - O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size(), curr = 0, candidate;
+        
+        for(int x:nums){
+            if(curr==0) candidate = x;
+            if(x==candidate) curr++;
+            else curr--;
+        }
+        
+        return candidate;
     }
 };
