c8a78e476d5d4df6db3100059ed1b7567adecdb6
Vivek Shah <vivekshahcodes@gmail.com>
2022-07-06 14:53:49 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,41 +1,65 @@
+//Brute Force
+//m - Number of unique triplets
+//TC - O((n^3)*log(m))
+//SC - O(m)
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
+        int n = nums.size();
         vector<vector<int>> ans;
+        set<vector<int>> s;
         
-        if(nums.size()==0){
-            return ans;
-        }
-        
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<nums.size()-1;i++){
-            
-            int l = i+1;
-            int r = nums.size()-1;
-            
-            while(l<r){
-                if(nums[l]+nums[r]==-nums[i]){
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    int a = nums[l];
-                    int b = nums[r];
-                    while(l<r && nums[l]==a){
-                        l++;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        vector<int> temp{nums[i], nums[j], nums[k]};
+                        sort(temp.begin(),temp.end());
+                        s.insert(temp);
                     }
-                    while(l<r && nums[r]==b){
-                        r--;
-                    }
-                }else if(nums[l]+nums[r]<-nums[i]){
-                    l++;
-                }else{
-                    r--;
                 }
             }
-            
-            while(i<nums.size()-1 && nums[i]==nums[i+1]){
-                i++;
+        }
+        
+        for(auto i:s) ans.push_back(i);
+        
+        return ans;
+    }
+};
+
+
+//Optimal Solution
+//TC - O(n*n)
+//SC - O(1)
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n = nums.size(), i = 0;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        
+        while(i<n){
+            int j = i+1, k = n-1;
+            int curr = nums[i];
+            while(j<n && k>=0 && j<k){
+                if(nums[j]+nums[k]==-curr){
+                    ans.push_back({curr,nums[j],nums[k]});
+                    j++;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    k--;
+                    while(k>j && nums[k]==nums[k+1]) k--;
+                }else if(nums[j]+nums[k]>-curr){
+                    k--;
+                }else{
+                    j++;
+                }
             }
+            i++;
+            while(i<n && nums[i]==nums[i-1]) i++;
         }
         
         return ans;
