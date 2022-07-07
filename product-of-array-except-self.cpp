596ffc91cf4df40c59633d1eea853685d1eac860
Vivek Shah <vivekshahcodes@gmail.com>
2022-07-07 11:02:52 +0530

    Update product-of-array-except-self.cpp

--- product-of-array-except-self.cpp
+++ product-of-array-except-self.cpp
@@ -1,21 +1,47 @@
+//Brute Force
+//TC - O(n^2)
+//SC - O(1)
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        vector<int> ans(nums.size(),1);
+        int n = nums.size(), leftProduct = 1;
+        vector<int> ans(n);
         
-        int temp  = 1;
-        
-        for(int i=0;i<nums.size();i++){
-            ans[i] *= temp;
-            temp*=nums[i];
+        for(int i=0;i<n;i++){
+            int rightProduct = 1, j = i+1;
+            while(j<n) rightProduct *= nums[j++];
+            ans[i] = leftProduct * rightProduct;
+            leftProduct *= nums[i];
         }
         
-        temp = 1;
+        return ans;
+    }
+};
+
+
+//Optimized Solution
+//TC - O(n)
+//SC - O(1)
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
         
-        for(int i=nums.size()-1;i>=0;i--){
-            ans[i] *= temp;
-            temp*=nums[i];
+        int n = nums.size();
+        vector<int> ans(n,1);
+        
+        int curr = 1;
+        for(int i=0;i<n;i++){
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        
+        curr = 1;
+        for(int i=n-1;i>=0;i--){
+            ans[i] *= curr;
+            curr *= nums[i];
         }
         
         return ans;
