ac7136d217dfaa88a172435dc4fd07c933524610
Vivek Shah <vivekshahcodes@gmail.com>
2022-07-09 12:17:36 +0530

    Update fibonacci-number.cpp

--- fibonacci-number.cpp
+++ fibonacci-number.cpp
@@ -6,10 +6,7 @@ class Solution {
 public:
     int fib(int n) {
         
-        if(n==0 || n==1){
-            return n;
-        }
-        
+        if(n<=1) return n;
         return fib(n-1)+fib(n-2);
     }
 };
@@ -24,14 +21,8 @@ public:
     
     int fibonacci(int n, vector<int>& dp){
         
-        if(n<=1){
-            return n;
-        }
-        
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
         return dp[n] = fibonacci(n-1,dp) + fibonacci(n-2,dp);
     }
     
@@ -55,9 +46,7 @@ public:
         dp[0] = 0;
         dp[1] = 1;
         
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        for(int i=2;i<=n;i++) dp[i] = dp[i-1] + dp[i-2];
         
         return dp[n];
     }
